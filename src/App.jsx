5d43c9818c10df41678eb884fab271388d7d56a9
divyasharma11 <divyasharma140802@gmail.com>
2023-05-08 17:45:06 +0530

    y

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const handleOnChange = (event)=>{
     <div className='main'>
      <h1>Text-Converter</h1>
      <div className="container">
-      <textarea value={text}  onChange={handleOnChange} cols="50" rows="10"></textarea>
+      <textarea value={text}  onChange={handleOnChange} cols="50"  id='text' rows="10"></textarea>
       </div>
      <div className="convert-btn">
      <button className='btn' onClick={changeToUpperCase}>Change to UpperCase</button>
